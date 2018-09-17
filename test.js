1b523d8940e2c0e1a0aca622f761f76c75961055
sstienface <s.stien@fondationface.org>
2018-09-17 10:23:14 +0200

    test

---
+++ test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by sstienface on 17/09/2018.
+ */
+
+alert("Bonjour cher visiteur");
+
+document.getElementById('monElementHTML').innerText = "Mon nouveau texte";
+
+function maFonction(prenom,nom)
+{
+    alert("Bonjour "+prenom+" "+nom);
+    document.getElementById('monElementHTML').innerText = "Mon nouveau texte";
+}
+
